a2ef41e335f5392a544dea7ec3541f68efe07400
haoz <haoz@tlen.pl>
2024-02-12 23:20:09 +0100

    Direct scale

--- src/main/java/edu/bergi/download/FileDownload.java
+++ src/main/java/edu/bergi/download/FileDownload.java
@@ -1,55 +1,26 @@
 package edu.bergi.download;
 
+import edu.bergi.imgutils.ImageScalar;
 import org.apache.commons.io.FileUtils;
-import org.asynchttpclient.*;
 
-import java.io.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.concurrent.ExecutionException;
 
 public class FileDownload {
 
-
-    public static void downloadWithJavaIO(String url, String localFilename) {
-
-        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream()); FileOutputStream fileOutputStream = new FileOutputStream(localFilename)) {
-
-            byte dataBuffer[] = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void downloadWithJava7IO(String url, String localFilename) {
-
-        try (InputStream in = new URL(url).openStream()) {
-            Files.copy(in, Paths.get(localFilename), StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void downloadWithJavaNIO(String fileURL, String localFilename) throws IOException {
-
+    public static void downloadWithImageIOAndScale(String fileURL, String localFilename, int imgWidth, int imgHeight) throws IOException {
         URL url = new URL(fileURL);
-        try (ReadableByteChannel readableByteChannel = Channels.newChannel(url.openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream(localFilename); FileChannel fileChannel = fileOutputStream.getChannel()) {
-
-            fileChannel.transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
-            fileOutputStream.close();
-        }
+        BufferedImage originalImage = ImageIO.read(url.openStream());
+        BufferedImage newResizedImage = ImageScalar.resizeImageQW(originalImage, imgWidth, imgHeight);
+        Path targetPath = Paths.get(localFilename);
+        String fileExtension = localFilename.substring(localFilename.lastIndexOf(".") + 1);
+        ImageIO.write(newResizedImage, fileExtension, targetPath.toFile());
     }
-
 
 
     public static void downloadWithApacheCommons(String url, String localFilename) {
@@ -64,29 +35,4 @@ public class FileDownload {
 
     }
 
-    public static void downloadWithAHC(String url, String localFilename) throws ExecutionException, InterruptedException, IOException {
-
-        FileOutputStream stream = new FileOutputStream(localFilename);
-        AsyncHttpClient client = Dsl.asyncHttpClient();
-
-        client.prepareGet(url)
-                .execute(new AsyncCompletionHandler<FileOutputStream>() {
-
-                    @Override
-                    public State onBodyPartReceived(HttpResponseBodyPart bodyPart) throws Exception {
-                        stream.getChannel()
-                                .write(bodyPart.getBodyByteBuffer());
-                        return State.CONTINUE;
-                    }
-
-                    @Override
-                    public FileOutputStream onCompleted(Response response) throws Exception {
-                        return stream;
-                    }
-                })
-                .get();
-
-        stream.getChannel().close();
-        client.close();
-    }
 }
